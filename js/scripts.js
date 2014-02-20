f870923c2d32a8f67ac51f042489edc59739a963
Mollie Gower <mgower@eagles.ewu.edu>
2014-02-20 13:26:27 -0800

    It WORKS!!

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,17 @@ $(document).foundation();
 // Your Awesome Scripts!
 $(document).ready(function(){
 
+		//Good Stuff
+
+new Maplace({
+	show_markers: false,
+	locations: [{
+		lat: 45.9, 
+		lon: 10.9,
+		zoom: 8
+			}]
+		}).Load(); 
+
 	console.log('Page Loaded. Lets Do this!');
 
 }); 
